8c7b22ccf5eb5f87abea28b361e3e4adbbc2875d
maximilien lehujeur <maximilien.lehujeur@gmail.com>
2020-01-06 17:13:59 +0100

    clean up

---
+++ proton/old/errors.py
@@ -0,0 +1,27 @@
+import sys
+import traceback
+import logging
+
+
+def error_message():
+    type, value, trace = sys.exc_info()
+    msg = ""
+    for level, tbb in enumerate(traceback.extract_tb(trace)):
+        file, line, fun, idiocy = tbb
+        tab = "".join(['  ' for i in range(level + 1)])
+        msg += '#%sIN %s, line %d, in %s : "%s"\n' % (tab, file, line, fun, idiocy)
+        level += 1
+    msg = '\n#ERROR : %s\n%s' % (value, msg[:-1])
+    return msg
+
+
+logger = logging.getLogger(__name__)
+
+try:
+    1 / 0
+except ZeroDivisionError as error:
+    logger.error(error)
+    raise                 # just this!
+    # raise AppError      # Don't do this, you'll lose the stack trace!
+finally:
+    print(logger)
