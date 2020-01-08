b3304f0ddb6862f48c2f4acf61b4b47d0ea3e0d3
maximilien lehujeur <maximilien.lehujeur@gmail.com>
2020-01-08 10:05:49 +0100

    version ++, custom init

--- proton/__init__.py
+++ proton/__init__.py
@@ -1,2 +1,4 @@
+from proton.printcolors import printblue, printyellow, printred
 from proton.jobs import Job
 from proton.mappers import MapAsync
+from proton.timelimiter import TimeLimiter
--- proton/version.py
+++ proton/version.py
@@ -3,5 +3,4 @@
 
 # please keep up-to-date with the tag name, this is the only place where to update the version number 
 
-global __version__
-__version__ = "v0.1"
+__version__ = "v1.0"
