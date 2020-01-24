41a0331d4f9ce2b13a36fec3d97622c1521826f7
maximilien lehujeur <maximilien.lehujeur@gmail.com>
2020-01-24 08:49:32 +0100

    import all printers by default

--- proton/__init__.py
+++ proton/__init__.py
@@ -1,4 +1,4 @@
-from proton.printcolors import printblue, printyellow, printred
+from proton.printcolors import printblue, printyellow, printred, printgreen, printpurple, printbold, printunderline
 from proton.jobs import Job
 from proton.mappers import MapAsync
 from proton.timelimiter import TimeLimiter
