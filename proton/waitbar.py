13f4981024485e7f3fd88b2de6ac00acc86fdafe
maximilien lehujeur <maximilien.lehujeur@gmail.com>
2020-01-24 08:49:09 +0100

    rename purcent to percent

--- proton/waitbar.py
+++ proton/waitbar.py
@@ -23,19 +23,19 @@ class WaitBar(object):
     symbol = '\u2588'
 
     def __str__(self):
-        s = "{:s} {:s} {:6.2f}% {:s}".format(self.title, self.bars(), 100. * self.purcent, self.remain)
+        s = "{:s} {:s} {:6.2f}% {:s}".format(self.title, self.bars(), 100. * self.percent, self.remain)
         return s 
 
     def bars(self):
-        nbars   = int(round(self.purcent * self.width))
+        nbars   = int(round(self.percent * self.width))
         nspaces = self.width - nbars
         return self.symbol * nbars + " " * nspaces
 
     def __init__(self, title="", width=40, reevaluatespeed=5.0):
         self.title = title
         self.width = width
-        self.purcent = 0.
-        self.lastpurcent = 0.
+        self.percent = 0.
+        self.lastpercent = 0.
         self.start = time.time()
         self.time = self.start
         self.lasttime = self.start
@@ -47,23 +47,23 @@ class WaitBar(object):
         sys.stdout.write(self.string)
         sys.stdout.flush()
 
-    def set_purcent(self, purcent):
-        if self.purcent < 0. or self.purcent > 1.:
+    def set_percent(self, percent):
+        if self.percent < 0. or self.percent > 1.:
             self.close()
-        # if purcent and purcent - 1. and abs(self.purcent - purcent) < 0.005:
+        # if percent and percent - 1. and abs(self.percent - percent) < 0.005:
         #   return
 
-        self.purcent, self.time = purcent, time.time()
+        self.percent, self.time = percent, time.time()
         if self.reevaluatespeed and self.time - self.lasttime > self.reevaluatespeed:
             # reevaluate speed every X seconds
-            self.speed = (self.purcent - self.lastpurcent) / (self.time - self.lasttime) 
-            self.lastpurcent, self.lasttime = self.purcent, self.time
+            self.speed = (self.percent - self.lastpercent) / (self.time - self.lasttime) 
+            self.lastpercent, self.lasttime = self.percent, self.time
 
         elif self.start == self.lasttime:
-            self.speed = (self.purcent - self.lastpurcent) / (self.time - self.lasttime) 
+            self.speed = (self.percent - self.lastpercent) / (self.time - self.lasttime) 
 
         if self.speed:
-            tremain = ((1. - self.purcent) / self.speed)
+            tremain = ((1. - self.percent) / self.speed)
             d = int(tremain / 24. / 3600.)
             h = int(tremain % (24. * 3600.) / 3600.)
             m = int(tremain % 3600. / 60.)
@@ -88,8 +88,8 @@ class WaitBar(object):
         sys.stdout.flush()
 
     def close(self):
-        self.purcent = 1.0-1.e-20
-        self.set_purcent(self.purcent)
+        self.percent = 1.0-1.e-20
+        self.set_percent(self.percent)
         sys.stdout.write("\n")
 
 
@@ -104,24 +104,24 @@ class MultiWaitbar(MultiLine):
     def __init__(self, linenames):
         super().__init__(maxlines=len(linenames))
         self.linenames = linenames
-        self.purcents = [0 for _ in range(self.maxlines)]
+        self.percents = [0 for _ in range(self.maxlines)]
 
     def __enter__(self):
         super().__enter__()
         for line in range(self.maxlines):
-            self.set_purcent(line, 0.)
+            self.set_percent(line, 0.)
         return self
 
-    def bars(self, purcent):
-        nbars   = int(round(purcent * self.width))
+    def bars(self, percent):
+        nbars   = int(round(percent * self.width))
         nspaces = self.width - nbars
         return self.symbol * nbars + " " * nspaces
 
-    def set_purcent(self, line_number, purcent):
+    def set_percent(self, line_number, percent):
         message = '{linename} {bars} {tail}'.format(
             linename=self.linenames[line_number],
-            bars=self.bars(purcent),
-            tail="{:.2f}%".format(purcent))
+            bars=self.bars(percent),
+            tail="{:.2f}%".format(percent))
 
         self.write(line_number=line_number, message=message)
 
@@ -130,14 +130,14 @@ if __name__ == "__main__":
 
     with MultiWaitbar(['worker 1:', 'worker 2:', 'worker 3:']) as mwb:
         for linenum in range(3)[::-1]:
-            for purcent in range(50):
-                mwb.set_purcent(linenum, purcent / 100.)
+            for percent in range(50):
+                mwb.set_percent(linenum, percent / 100.)
 
                 time.sleep(0.01)
 
-        for purcent in range(50, 100):
+        for percent in range(50, 100):
             for linenum in range(3)[::-1]:
-                mwb.set_purcent(linenum, purcent / 100.)
+                mwb.set_percent(linenum, percent / 100.)
 
                 time.sleep(0.01)
 
@@ -152,17 +152,17 @@ if __name__ == "__main__":
     for i in range(100):
         time.sleep(0.01)
         if i and not i % 3:
-            # set_purcent every 3 iterations
-            w.set_purcent(i / 100.)
-    w.set_purcent(1.)
+            # set_percent every 3 iterations
+            w.set_percent(i / 100.)
+    w.set_percent(1.)
     w.close()
 
     w = WaitBarPipe('progress')
     for i in range(100):
         time.sleep(0.01)
         if i and not i % 3:
-            # set_purcent every 3 iterations
-            w.set_purcent(i / 100.)
-    w.set_purcent(1.)
+            # set_percent every 3 iterations
+            w.set_percent(i / 100.)
+    w.set_percent(1.)
     w.close()
 
