4fcf0c26f77e534811ea9112c3526e3dcec12711
maximilien lehujeur <maximilien.lehujeur@gmail.com>
2020-09-01 23:40:17 +0200

    add MapSync to init

--- proton/__init__.py
+++ proton/__init__.py
@@ -1,4 +1,4 @@
 from proton.printcolors import printblue, printyellow, printred, printgreen, printpurple, printbold, printunderline
 from proton.jobs import Job
-from proton.mappers import MapAsync
+from proton.mappers import MapAsync, MapSync
 from proton.timelimiter import TimeLimiter
