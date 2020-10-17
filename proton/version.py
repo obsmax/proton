2f7036dd48c82414db84217a5a123d9d7cc5f5ac
maximilien lehujeur <maximilien.lehujeur@gmail.com>
2020-10-17 14:11:06 +0200

    fix version policy and version++

--- proton/version.py
+++ proton/version.py
@@ -1,6 +1,1 @@
-# set the version number of this program, 
-# after this source https://packaging.python.org/guides/single-sourcing-package-version/
-
-# please keep up-to-date with the tag name, this is the only place where to update the version number 
-
-__version__ = "v1.2.2"
+__version__ = "v1.2.3"
--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 import setuptools
-from distutils.core import setup
-from proton.version import __version__
+import os
+
+# fuck distutils2
+version_file = os.path.join('proton', 'version.py')
+if not os.path.isfile(version_file):
+    raise IOError(version_file)
+
+with open(version_file, "r") as fid:
+    for l in fid:
+        if l.strip('\n').strip().startswith('__version__'):
+            __version__ = l.strip('\n').split('=')[-1].split()[0].strip()
+            break
+    else:
+        raise Exception(f'could not detect __version__ affectation in {version_file}')
 
 with open("Readme.md", "r") as fh:
     long_description = fh.read()
 
-setup(
+setuptools.setup(
     name='proton',
     author = "Maximilien Lehujeur",
     author_email = "maximilien.lehujeur@gmail.com",
@@ -20,4 +32,5 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3"
         "Operating System :: Linux"],
+    install_requires=['numpy', ],
     python_requires='>=3')
